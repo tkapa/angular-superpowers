8931c494553700cfce6fa84641c800161e7dfbcb
Tylah Kapa <tylahkapa@gmail.com>
2024-01-15 10:23:36 +1000

    Create FBC Company List Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import {
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CompanyListComponent } from './company/company-list/company-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CompanyListComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
---
+++ src/app/company/company-list/company-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbc-company-list',
+  templateUrl: './company-list.component.html',
+  styleUrl: './company-list.component.scss'
+})
+export class CompanyListComponent {
+
+}
